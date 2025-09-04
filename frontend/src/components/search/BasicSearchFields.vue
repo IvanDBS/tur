36a916a47e3e1bfb74063baa0927ec561b61f970
Ivan Teaca <ivan.teaca@gmail.com>
2025-09-04 19:41:53 +0300

    fix: arrow position in labels

--- frontend/src/components/search/BasicSearchFields.vue
+++ frontend/src/components/search/BasicSearchFields.vue
@@ -2,57 +2,65 @@
   <div class="form-row">
     <!-- Откуда -->
     <div class="field-group">
+      <label class="field-label">
+        <span v-if="activeSelector === 'departureCity'" class="field-arrow"></span>
+        Откуда:
+      </label>
       <BaseSelect
         v-model="localForm.departureCity"
         :options="departureCitiesOptions"
-        label="Откуда:"
         placeholder="Выберите город"
         :disabled="isLoading"
         :can-clear="false"
         :can-deselect="false"
         :searchable="true"
       />
-      <span v-if="activeSelector === 'departureCity'" class="field-arrow"></span>
     </div>
 
     <!-- Куда -->
     <div class="field-group" :class="{ 'disabled-field': !localForm.departureCity }">
+      <label class="field-label">
+        <span v-if="activeSelector === 'destination'" class="field-arrow"></span>
+        Куда:
+      </label>
       <BaseSelect
         v-model="localForm.destination"
         :options="countriesOptions"
-        label="Куда:"
         placeholder="Выберите страну"
         :disabled="isLoading || !localForm.departureCity"
         :can-clear="false"
         :can-deselect="false"
         :searchable="true"
       />
-      <span v-if="activeSelector === 'destination'" class="field-arrow"></span>
     </div>
 
     <!-- Пакет -->
     <div class="field-group" :class="{ 'disabled-field': !localForm.destination }">
+      <label class="field-label">
+        <span v-if="activeSelector === 'package'" class="field-arrow"></span>
+        Пакет:
+      </label>
       <BaseSelect
         v-model="localForm.package"
         :options="packagesOptions"
-        label="Пакет:"
         placeholder="Выберите пакет"
         :disabled="isLoading || !localForm.destination"
         :can-clear="false"
         :can-deselect="false"
         :searchable="true"
       />
-      <span v-if="activeSelector === 'package'" class="field-arrow"></span>
     </div>
 
     <!-- Город прилета -->
     <div class="field-group" :class="{ 'disabled-field': !localForm.package }">
+      <label class="field-label">
+        <span v-if="activeSelector === 'arrivalCity'" class="field-arrow"></span>
+        Город прилета:
+      </label>
       <BaseInput
         :model-value="localForm.arrivalCity ? localForm.arrivalCity.name : 'Город будет выбран автоматически'"
-        label="Город прилета:"
         :disabled="true"
       />
-      <span v-if="activeSelector === 'arrivalCity'" class="field-arrow"></span>
     </div>
   </div>
 </template>
@@ -108,16 +116,24 @@ watch(() => props.modelValue, (newValue) => {
   gap: 2px;
 }
 
+.field-label {
+  font-size: 11px;
+  font-weight: 600;
+  color: #222222;
+  margin-bottom: 2px;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+}
+
 /* Переопределяем стили BaseInput для соответствия дизайну формы */
 .field-group :deep(.form-field) {
   margin-bottom: 0;
 }
 
+/* Скрываем label в BaseInput и BaseSelect, так как используем собственные */
 .field-group :deep(.form-field__label) {
-  font-size: 11px;
-  font-weight: 600;
-  color: #222222;
-  margin-bottom: 2px;
+  display: none;
 }
 
 .field-group :deep(.form-field__input) {
@@ -143,7 +159,7 @@ watch(() => props.modelValue, (newValue) => {
 }
 
 /* Стили для неактивных полей */
-.field-group.disabled-field :deep(.form-field__label) {
+.field-group.disabled-field .field-label {
   color: #999999 !important;
 }
 
